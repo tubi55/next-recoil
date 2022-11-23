e5c52a57ccf7efe2a40cfb90584f964ebae00aec
tubi55 <tubi55@nate.com>
2022-11-23 23:41:09 +0900

    layout컴포넌트 분리

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,13 @@
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import Layout from '../components/Layout';
+import '../styles/globals.css';
 
 function App({ Component, pageProps }) {
 	return (
-		<>
-			<Header />
+		<Layout>
 			<Component {...pageProps} />
-			<Footer />
-			<style jsx global>{`
-				* {
-					margin: 0;
-					padding: 0;
-					box-sizing: border-box;
-				}
-				ul,
-				ol,
-				li {
-					list-style: none;
-				}
-				a {
-					text-decoration: none;
-				}
-				main {
-					min-height: 700px;
-					padding: 100px 5vw;
-				}
-			`}</style>
-		</>
+		</Layout>
 	);
 }
 
